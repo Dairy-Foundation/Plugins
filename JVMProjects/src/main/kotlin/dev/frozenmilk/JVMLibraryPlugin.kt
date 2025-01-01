6e31489ad9d7f8734ea044efec511592f06fd671
Oscar Chevalier <oscar.chevalier@gmail.com>
2025-01-02 00:44:06 +1100

    feat: updated JVMLibraryPlugin

--- JVMProjects/src/main/kotlin/dev/frozenmilk/JVMLibraryPlugin.kt
+++ JVMProjects/src/main/kotlin/dev/frozenmilk/JVMLibraryPlugin.kt
@@ -3,23 +3,34 @@ package dev.frozenmilk
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginExtension
-import org.gradle.jvm.toolchain.JavaLanguageVersion
-import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformJvmPlugin
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
 
 @Suppress("unused")
 class JVMLibraryPlugin : Plugin<Project> {
 	override fun apply(project: Project) {
 		with(project) {
-			plugins.apply(KotlinPlatformJvmPlugin::class.java)
+			// note: "org.jetbrains.kotlin.jvm"
+			plugins.apply(KotlinPluginWrapper::class.java)
 			plugins.apply("java-library")
-		}
 
-		project.extensions.getByType(JavaPluginExtension::class.java).run {
-			toolchain {
-				it.languageVersion.set(JavaLanguageVersion.of(8))
+			repositories.run {
+				google()
+				mavenCentral()
 			}
+
+			extensions.getByType(KotlinJvmProjectExtension::class.java).run {
+				jvmToolchain(8)
+				compilerOptions {
+					freeCompilerArgs.add("-Xjvm-default=all")
+				}
+			}
+
+			extensions.getByType(JavaPluginExtension::class.java).run {
+				withSourcesJar()
+			}
+
+			dependencies.add("testImplementation", "junit:junit:4.13.2")
 		}
-
-
 	}
 }
